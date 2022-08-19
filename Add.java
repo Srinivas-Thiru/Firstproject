6f299578185ac9cf8d118623d16b27bbde0c66c2
SRI0000000 <110653801+SRI0000000@users.noreply.github.com>
2022-08-19 20:11:56 +0530

    changed a to 222

--- Add.java
+++ Add.java
@@ -1,6 +1,6 @@
 public class Add {
     public static void main(String[] args) {
-        int a = 10, b = 10;
+        int a = 222, b = 10;
         int c = a + b;
         System.out.println("The sum is " + c);
     }
