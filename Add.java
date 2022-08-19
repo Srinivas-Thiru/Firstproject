7fe5a0b4369675adef0f1b75c478fb95b9a580e0
Srinivas <nivasthiru22@gmail.com>
2022-08-19 20:19:23 +0530

    updated a to 15

--- Add.java
+++ Add.java
@@ -1,10 +1,6 @@
 public class Add {
     public static void main(String[] args) {
-<<<<<<< HEAD
         int a = 15, b = 10;
-=======
-        int a = 222, b = 10;
->>>>>>> 6f299578185ac9cf8d118623d16b27bbde0c66c2
         int c = a + b;
         System.out.println("The sum is " + c);
     }
