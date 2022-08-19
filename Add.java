1ac9a2424c6dddb1c4d9f45f3d37d38fd2378eae
Srinivas <nivasthiru22@gmail.com>
2022-08-19 20:50:15 +0530

    result of add and multiply

--- Add.java
+++ Add.java
@@ -2,7 +2,9 @@ public class Add {
     public static void main(String[] args) {
         int a = 15, b = 10;
         int c = a * b;
-        System.out.println("The sum is " + c);
+        int d = a + b;
+        System.out.println("The sum is " + d);
+        System.out.println("The multiplied answer is " + c);
     }
     
 }
