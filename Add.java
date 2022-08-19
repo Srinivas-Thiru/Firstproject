99e56f74864d79be9e37bb105e97a7fd23e82eff
Srinivas <nivasthiru22@gmail.com>
2022-08-19 20:53:02 +0530

    difference is added

--- Add.java
+++ Add.java
@@ -3,8 +3,10 @@ public class Add {
         int a = 15, b = 10;
         int c = a * b;
         int d = a + b;
+        int e = a - b;
         System.out.println("The sum is " + d);
         System.out.println("The multiplied answer is " + c);
+        System.out.println("The difference is " + e);
     }
     
 }
