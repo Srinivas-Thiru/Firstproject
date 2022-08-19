dd84242e589d54ae2dfdd608d5be9eae67b3f9de
SRI0000000 <110653801+SRI0000000@users.noreply.github.com>
2022-08-19 20:04:04 +0530

    changed a and b

--- Add.java
+++ Add.java
@@ -1,6 +1,6 @@
 public class Add {
     public static void main(String[] args) {
-        int a = 1, b = 1;
+        int a = 10, b = 10;
         int c = a + b;
         System.out.println("The sum is " + c);
     }
